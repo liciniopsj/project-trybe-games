062d57dea5fbde72d1f5e24feb792d2be911059f
liciniopsj <licinio.psj@gmail.com>
2023-07-19 22:18:48 -0300

    Req 04 - 05 / Linq

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using System.Linq.Expressions;
+
 namespace TrybeGames;
 
 public class TrybeGamesDatabase
@@ -11,15 +14,20 @@ public class TrybeGamesDatabase
     // 4. Crie a funcionalidade de buscar jogos desenvolvidos por um estúdio de jogos
     public List<Game> GetGamesDevelopedBy(GameStudio gameStudio)
     {
-        // implementar
-        throw new NotImplementedException();
+        List<Game> filteredGames =
+                                (from games in Games
+                                 where games.DeveloperStudio == gameStudio.Id
+                                 select games).ToList();
+        return filteredGames;
     }
 
     // 5. Crie a funcionalidade de buscar jogos jogados por uma pessoa jogadora
     public List<Game> GetGamesPlayedBy(Player player)
     {
-        // Implementar
-        throw new NotImplementedException();  
+        List<Game> filteredGames = (from games in Games
+                                where games.Players.Contains(player.Id)
+                                select games).ToList();
+        return filteredGames;
     }
 
     // 6. Crie a funcionalidade de buscar jogos comprados por uma pessoa jogadora
@@ -34,9 +42,9 @@ public class TrybeGamesDatabase
     public List<GameWithStudio> GetGamesWithStudio()
     {
         // Implementar
-        throw new NotImplementedException();                      
+        throw new NotImplementedException();
     }
-    
+
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
